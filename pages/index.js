ef7d39f64bed3204d85164d84b0fe0dc66bd71f0
Med D. Bouthuri <m.dhia.bouthuri@gmail.com>
2022-02-02 13:37:23 +0100

    tailwind scrollbar hide

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import Nav from "../components/nav"
 
 export default function Home() {
     return (
@@ -13,7 +14,7 @@ export default function Home() {
 
 
             <Header/>
-
+            <Nav/>
         </div>
     )
 }
